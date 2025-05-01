d6964343cc7e8453337e32546e0fed369ad1d629
amagarola <adrianmagarola@gmail.com>
2025-05-01 15:36:28 +0200

    Fix external kubeconfig for workers

--- terraform/data.tf
+++ terraform/data.tf
@@ -4,13 +4,13 @@ data "external" "kubeconfig" {
   program = [
     "sh", "-c",
     <<-EOT
-  ssh -o StrictHostKeyChecking=no -i modules/k3s-cluster/k3s-key.pem ubuntu@${module.k3s_cluster.master_public_ip} '
-    CA=$(sudo cat /etc/rancher/k3s/k3s.yaml | grep certificate-authority-data | awk "{print \\$2}")
-    CERT=$(sudo cat /etc/rancher/k3s/k3s.yaml | grep client-certificate-data | awk "{print \\$2}")
-    KEY=$(sudo cat /etc/rancher/k3s/k3s.yaml | grep client-key-data | awk "{print \\$2}")
-    echo "{\"ca\":\"$CA\",\"cert\":\"$CERT\",\"key\":\"$KEY\"}"
-  '
-  EOT
+    ssh -o StrictHostKeyChecking=no -i modules/k3s-cluster/k3s-key.pem ubuntu@${module.k3s_cluster.master_public_ip} '
+      yaml=/etc/rancher/k3s/k3s.yaml
+      CA=$(grep certificate-authority-data "$yaml" | sed "s/.*: //")
+      CERT=$(grep client-certificate-data "$yaml" | sed "s/.*: //")
+      KEY=$(grep client-key-data "$yaml" | sed "s/.*: //")
+      echo "{\"ca\":\"$CA\",\"cert\":\"$CERT\",\"key\":\"$KEY\"}"
+    '
+    EOT
   ]
-
 }
