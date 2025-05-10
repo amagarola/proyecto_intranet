60c4059eea84557ef12bdbc116bbaf4da4c252ed
AdriBFK <adrianmagarola@gmail.com>
2025-05-11 01:20:02 +0200

    fix: remove commented-out code and streamline DNS records in terraform configuration

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -6,24 +6,6 @@ data "aws_route53_zone" "main" {
   name = "adrianmagarola.click."
 }
 
-# data "kubernetes_service" "traefik_lb" {
-#   metadata {
-#     name      = "traefik"
-#     namespace = "kube-system"
-#   }
-# }
-##########################
-# Crear registro raíz (dominio principal)
-##########################
-
-# resource "aws_route53_record" "root" {
-#   zone_id = data.aws_route53_zone.main.zone_id
-#   name    = var.domains[0]
-#   type    = "A"
-#   ttl     = 60
-#   records = [module.proxy.proxy_public_ip]
-# }
-
 ##########################
 # Crear subdominio para ArgoCD
 ##########################
@@ -37,12 +19,14 @@ resource "aws_route53_record" "domains" {
   records  = [module.proxy.proxy_public_ip]
 }
 
-# resource "aws_route53_record" "proxy_domains" {
-#   for_each = toset(var.domains)
+##########################
+# Crear registro para K3s Master
+##########################
 
-#   zone_id = var.zone_id
-#   name    = each.value
-#   type    = "A"
-#   ttl     = 60
-#   records = [module.proxy.proxy_public_ip]
-# }
+resource "aws_route53_record" "k3s_master" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "k3s.adrianmagarola.click"
+  type    = "A"
+  ttl     = 60
+  records = [module.k3s_cluster.master_public_ip]
+}
