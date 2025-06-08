5eff612a59f763dd5956a700762546a5ac0a1e5a
AdriBFK <adrianmagarola@gmail.com>
2025-06-09 01:32:44 +0200

    feat: add persistent volume claim for Grafana and update deployment for storage

--- terraform/modules/proxy/main.tf
+++ terraform/modules/proxy/main.tf
@@ -122,6 +122,12 @@ resource "aws_security_group" "this" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -86,6 +86,12 @@ resource "aws_security_group" "k3s_sg" {
     protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/8"]
   }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   #######################################################
   # EGRESS  (todo permitido para que los nodos salgan a Internet)
