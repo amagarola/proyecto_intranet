b9377e31c9c6b6ee9da0d51054e7552440c9ec5b
amagarola <adrianmagarola@gmail.com>
2025-06-08 15:36:51 +0200

    refactor: migrate aws_security_group for proxy to security_groups.tf and remove from main.tf; add variable for proxy resource name

--- terraform/modules/proxy/main.tf
+++ terraform/modules/proxy/main.tf
@@ -105,35 +105,4 @@ EOF
 
 }
 
-resource "aws_security_group" "this" {
-  name        = "${var.name}-sg"
-  description = "Allow HTTP and HTTPS"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "Allow HTTPS traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+# Se eliminó el recurso aws_security_group.this porque fue migrado a security_groups.tf
